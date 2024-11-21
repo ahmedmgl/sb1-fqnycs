47220185c1ba92ee3c37a131075991e0843482b7
ahmedmgl <blackartz59@gmail.com>
2024-11-21 22:21:55 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-});
+  base: '/sb1-fqnycs/', // Ajoutez ceci - doit correspondre au nom de votre dépôt
+  plugins: [react()]
+})
